a81ec99ff29052b22e679bdc35c4c5745c143afb
daniel.ducuara <daniel.ducuara@outscale.com>
2024-07-29 08:25:35 +0000

    small format refactor

--- src/components/GetTextAudio.js
+++ src/components/GetTextAudio.js
@@ -1,6 +1,6 @@
-import React, { Component, createRef } from 'react';
+import React, { Component, createRef } from "react";
 import Api from "../Api";
-import './GetTextAudio.css';
+import "./GetTextAudio.css";
 
 class GetTextAudio extends Component {
   constructor(props) {
@@ -11,10 +11,11 @@ class GetTextAudio extends Component {
       transcription: null,
       transcriptionFilename: null,
       downloadaleText: null,
-      error: null,
+      errorUploading: null,
+      errorTranscribing: null,
       isUploading: false,
       isProcessing: false,
-      accuracyMode: 'medium',
+      accuracyMode: "medium",
     };
     // this.audioRef = createRef();
     this.fileCheckInterval = null;
@@ -32,69 +33,90 @@ class GetTextAudio extends Component {
   handleModeChange(event) {
     this.setState({ accuracyMode: event.target.value });
   }
-  
+
   handleUpload() {
     const { selectedFile } = this.state;
     if (!selectedFile) {
-      this.setState({ error: 'Please select an audio file to upload.' });
+      this.setState({
+        errorUploading: "Please select an audio file to upload.",
+      });
       return;
     }
     const formData = new FormData();
-    formData.append('file', selectedFile);
-    
+    formData.append("file", selectedFile);
+
     this.setState({ isUploading: true });
-    Api.post('audio/upload', formData, {
+    Api.post("audio/upload", formData, {
       headers: {
-        'Content-Type': 'multipart/form-data'
-      }
+        "Content-Type": "multipart/form-data",
+      },
     })
-    .then(response => {
-      const uploadedFile = response.data.filename;
-      console.log(uploadedFile);
-      this.setState({ uploadedFile, error: null, isUploading: false }, () => {
-        this.getAudioData(uploadedFile);
+      .then((response) => {
+        const uploadedFile = response.data.filename;
+        console.log(uploadedFile);
+        this.setState(
+          { uploadedFile, errorUploading: null, isUploading: false },
+          () => {
+            this.getAudioData(uploadedFile);
+          }
+        );
+      })
+      .catch((error) => {
+        this.setState({
+          errorUploading: "Error uploading file. Please try again.",
+          isUploading: false,
+        });
       });
-    })
-    .catch(error => {
-      this.setState({ error: 'Error uploading file. Please try again.', isUploading: false });
-    });
   }
 
   handleTranscription() {
     const { uploadedFile, accuracyMode } = this.state;
     if (!uploadedFile) {
-      this.setState({ error: 'No audio file uploaded.' });
+      this.setState({ errorTranscribing: "No audio file uploaded." });
       return;
     }
 
-    console.log(uploadedFile)
+    console.log(uploadedFile);
     this.setState({ isProcessing: true });
-    Api.post('audio/transcribe', { filename: uploadedFile, mode: accuracyMode })
-    .then(response => {
-      const transcriptionFilename = response.data.transcription_filename;
-      this.setState({ transcriptionFilename });
-      this.getTranscriptionData(transcriptionFilename);
-      
-    })
-    .catch(error => {
-      this.setState({ error: 'Error getting transcription. Please try again.', isProcessing: false });
-    });
+    Api.post("audio/transcribe", { filename: uploadedFile, mode: accuracyMode })
+      .then((response) => {
+        const transcriptionFilename = response.data.transcription_filename;
+        this.setState({ transcriptionFilename, errorTranscribing: null });
+        this.getTranscriptionData(transcriptionFilename);
+      })
+      .catch((error) => {
+        this.setState({
+          errorTranscribing: "Error getting transcription. Please try again.",
+          isProcessing: false,
+        });
+      });
   }
 
   getTranscriptionData = (transcriptionFilename) => {
     const checkTranscriptionFile = () => {
       Api.get(`audio/transcription?filename=${transcriptionFilename}`)
-      .then((response) => {
-        const { transcription } = response.data;
-        if (transcription) {
-          this.setState({ transcription, isProcessing: false, downloadaleText: new Blob([transcription], {type: 'text/plain'})});
+        .then((response) => {
+          const { transcription } = response.data;
+          if (transcription) {
+            this.setState({
+              transcription,
+              errorTranscribing: null,
+              isProcessing: false,
+              downloadaleText: new Blob([transcription], {
+                type: "text/plain",
+              }),
+            });
+            clearInterval(this.fileCheckInterval);
+          }
+        })
+        .catch((error) => {
+          this.setState({
+            errorTranscribing:
+              "Error checking transcription file. Please try again.",
+            isProcessing: false,
+          });
           clearInterval(this.fileCheckInterval);
-        }
-      })
-      .catch((error) => {
-        this.setState({ error: 'Error checking transcription file. Please try again.', isProcessing: false });
-        clearInterval(this.fileCheckInterval);
-      });
+        });
     };
 
     this.fileCheckInterval = setInterval(checkTranscriptionFile, 5000);
@@ -103,36 +125,58 @@ class GetTextAudio extends Component {
 
   getAudioData = (audioFilename) => {
     Api.get(`audio/data?filename=${audioFilename}`)
-    .then((response) => {
-      // console.log(response.data)
-      // const audioData = URL.createObjectURL(new Blob([response.data], { type: 'audio/mpeg' }));
-      // console.log(this.audioRef)
-      // if (this.audioRef.current) {
-      //   this.audioRef.current.src = audioData;
-      //   this.audioRef.current.load();
-      //   this.audioRef.current.play();
-      // }
-    })
-    .catch((error) => {
-      this.setState({ error: 'Error checking transcription file. Please try again.', isProcessing: false });
-    });
+      .then((response) => {
+        // console.log(response.data)
+        // const audioData = URL.createObjectURL(new Blob([response.data], { type: 'audio/mpeg' }));
+        // console.log(this.audioRef)
+        // if (this.audioRef.current) {
+        //   this.audioRef.current.src = audioData;
+        //   this.audioRef.current.load();
+        //   this.audioRef.current.play();
+        // }
+      })
+      .catch((error) => {
+        this.setState({
+          errorUploading: "Error getting audio data to play it.",
+          isProcessing: false,
+        });
+      });
   };
 
   render() {
-    const { selectedFile, uploadedFile, transcriptionFilename, transcription, downloadaleText, error, isUploading, isProcessing, accuracyMode } = this.state;
+    const {
+      selectedFile,
+      uploadedFile,
+      transcriptionFilename,
+      transcription,
+      downloadaleText,
+      errorUploading,
+      errorTranscribing,
+      isUploading,
+      isProcessing,
+      accuracyMode,
+    } = this.state;
 
     return (
       <div className="container">
         <h2>Transcribe Audio</h2>
         <label className="label-file">
           Choose Audio File
-          <input type="file" onChange={this.handleFileChange} accept="audio/*" className="input-file" />
+          <input
+            type="file"
+            onChange={this.handleFileChange}
+            accept="audio/*"
+            className="input-file"
+          />
         </label>
-        {selectedFile && (
-          <p className="filename">{selectedFile.name}</p>
-        )}
-        <button onClick={this.handleUpload} disabled={isUploading || isProcessing}>Upload Audio</button>
-        {error && <p className="error">{error}</p>}
+        {selectedFile && <p className="filename">{selectedFile.name}</p>}
+        <button
+          onClick={this.handleUpload}
+          disabled={isUploading || isProcessing}
+        >
+          Upload Audio
+        </button>
+        {errorUploading && <p className="error">{errorUploading}</p>}
         {uploadedFile && (
           <div>
             <p>Uploaded Audio: {selectedFile.name}</p>
@@ -151,16 +195,25 @@ class GetTextAudio extends Component {
                   <option value="large">Large</option>
                 </select>
               </div>
-              <button onClick={this.handleTranscription} disabled={isUploading || isProcessing}>Get Text from Audio</button>
+              <button
+                onClick={this.handleTranscription}
+                disabled={isUploading || isProcessing}
+              >
+                Get Text from Audio
+              </button>
             </div>
           </div>
         )}
         {isProcessing && <p>Processing audio, please wait...</p>}
-        {error && <p className="error">{error}</p>}
+        {errorTranscribing && <p className="error">{errorTranscribing}</p>}
         {transcription && (
           <div className="transcription">
             <h3>Transcription Ready:</h3>
-            <a href={URL.createObjectURL(downloadaleText)} download={transcriptionFilename} className="button">
+            <a
+              href={URL.createObjectURL(downloadaleText)}
+              download={transcriptionFilename}
+              className="button"
+            >
               Download Transcription
             </a>
             <textarea readOnly value={transcription} />
